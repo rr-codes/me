59dc4a54bdbd40db41056d75dd019b2e8b64ad2b
Richard Robinson <rir@my.yorku.ca>
2021-03-04 21:32:53 -0500

    updated about info

--- models/about_info.go
+++ models/about_info.go
@@ -8,19 +8,21 @@ type Location struct {
 type AboutInfo struct {
 	Name     string            `json:"name"`
 	Location Location          `json:"location"`
-	Links    map[string]string `json:"links"`
 	Details  string            `json:"details"`
+	Links    map[string]string `json:"links"`
+	Email    string            `json:"email"`
 }
 
 func GetAboutInfo() AboutInfo {
 	return AboutInfo{
 		Name:     "Richard Robinson",
 		Location: Location{City: "Toronto", Country: "Canada"},
+		Details:  "I'm a full-stack software engineer in Toronto, and currently in my fourth year at University",
 		Links: map[string]string{
-			"twitter":  "twitter.com/rr_codes",
-			"github":   "github.com/rr_codes",
-			"linkedin": "",
+			"twitter":  "https://twitter.com/rr_codes",
+			"github":   "https://github.com/rr-codes",
+			"linkedin": "https://linkedin.com/in/richard-robinson-1694a7172/",
 		},
-		Details: "I'm a full-stack software engineer in Toronto, and currently in my fourth year at University",
+		Email: "richard@rr.codes",
 	}
 }
